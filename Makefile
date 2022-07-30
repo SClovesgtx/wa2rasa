cc21636e1f5a5339839c65e30dd3a4d5fd0a82a0
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2022-07-30 15:52:14 -0300

    feat: intents and entities parses

---
+++ Makefile
@@ -0,0 +1,59 @@
+# The present file, 'Makefile' has been modified from the original at
+# https://github.com/NeowayLabs/data-science-template
+# under the folllowing license:
+#
+# MIT License
+#
+# Copyright (c) 2019 Neoway Business Solution
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# BUILD = docker-compose build
+# RUN = docker-compose run
+# UP = docker-compose up
+# DOWN = docker-compose down
+# VERSION = $(shell awk -F ' = ' '$$1 ~ /version/ { gsub(/[\"]/, "", $$2); printf("%s",$$2) }' version.toml)
+
+help:
+	@echo "USAGE"
+	@echo
+	@echo "    make <command>"
+	@echo "    Include 'sudo' when necessary."
+	@echo "    To avoid using sudo, follow the steps in"
+	@echo "    https://docs.docker.com/engine/install/linux-postinstall/"
+	@echo
+	@echo
+	@echo "COMMANDS"
+	@echo
+	@echo "    code-format                  aplly black code formatter."
+	@echo "    cyclomatic-complexity        metric for the complexity of our code."
+	@echo "    test                         run all unit tests."
+
+#################
+# User Commands #
+#################
+
+code-format:
+	$ black wa2rasa/
+
+cyclomatic-complexity:
+	$ radon cc wa2rasa/wa2rasa/*.py -a
+
+test:
+	$ pytest wa2rasa/tests/test_wa2rasa.py --rootdir wa2rasa/
--- wa2rasa/wa2rasa/cli.py
+++ wa2rasa/wa2rasa/cli.py
@@ -1,37 +1,64 @@
 import json
-from typing import Optional
+import pathlib
 
 import typer
 from rich.console import Console
+from rich.theme import Theme
 
-from wa2rasa import read_wa_object
+from wa2rasa import read_wa_object, intents_parser, entities_parser, save_rasa_yaml_file
 
-app = typer.Typer(name="wa2rasa", 
-    add_completion=False, 
-    help="Convert Watson Assistant skill object to rasa nlu.yml file.")
+app = typer.Typer(
+    name="wa2rasa",
+    add_completion=False,
+    help="Convert Watson Assistant skill object to rasa nlu.yml file.",
+)
 
-console = Console()
+custom_theme = Theme({"info": "dim cyan", "warning": "magenta", "danger": "bold red"})
+console = Console(theme=custom_theme)
+
 
 @app.command()
 def introduce_app(name: str = typer.Argument("bro", help="Your name.")):
     """App intro."""
     console.print(
         f"Hey {name}! This app helps you to convert your watson Assistant skill object to rasa nlu.yml file. For that, use the argument 'convert'.",
-        style="green"
+        style="info",
     )
 
+
 @app.command()
-def convert(wa_file: str = typer.Argument(..., help="The path to your Watson Assistant json file."),
-            save_in: str = typer.Argument(..., help="Path to the directory where you want to store the output.")):
+def convert(
+    wa_file: str = typer.Argument(
+        ..., help="The path to your Watson Assistant json file."
+    ),
+    save_in: str = typer.Argument(
+        ..., help="Path to the directory where you want to store the output."
+    ),
+    rasa_version: str = typer.Argument("3.1", help="The version of your rasa app."),
+):
     """
     Convert Watson Assistant skill object to rasa nlu.yml file.
     """
     wa_object = read_wa_object(file_path=wa_file)
-    # Do pipeline here
+    intents = wa_object.get("intents", [])
+    entities = wa_object.get("entities", [])
+    if intents:
+        intents = intents_parser(intents)
+    if entities:
+        entities = entities_parser(entities)
+    nlu_yml_obj = {"version": rasa_version, "nlu": intents + entities}
+    if not entities:
+        console.print(
+            "[warning]Warning: No entity found in your Watson Assistant object.[/warning]"
+        )
+    if not intents:
+        console.print(
+            "[warning]Warning: No entities found in your Watson Assistant object.[/warning]"
+        )
+    file_path = pathlib.Path(save_in, "nlu.yml")
+    save_rasa_yaml_file(file_path=file_path, object=nlu_yml_obj)
+    console.print(f"Output saved in '{file_path}'.", style="info")
 
-    # output
-    print(json.dumps(wa_object, sort_keys=False, indent=2))
-    print(f"Output saved in {save_in}")
 
 if __name__ == "__main__":
     app()
